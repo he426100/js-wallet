5f253c96f51e028d136cf267f9f5c2cd2ad1b377
he426100 <9689137+he426100@users.noreply.github.com>
2022-06-02 10:53:50 +0800

    Update fil.js

--- fil.js
+++ fil.js
@@ -1,5 +1,5 @@
 import bip39 from 'bip39'
-import rustModule from '@zondax/filecoin-signing-tools/js'
+import filecoinSigner from '@zondax/filecoin-signing-tools/js'
 import createPath from './utils/fil/createPath.js'
 import { MAINNET_PATH_CODE } from './utils/fil/constants.js'
 
@@ -8,15 +8,9 @@ const mnemonic = bip39.generateMnemonic()
 // const mnemonic = 'motion double coconut narrow rather call genius secret magnet gap slot fog'
 
 const networkCode = MAINNET_PATH_CODE
-const privateKeyObj = rustModule.keyDerive(mnemonic, createPath(networkCode, 0), '')
-const privateKey = privateKeyObj.private_hexstring
-const address = privateKeyObj.address
-
-console.log(privateKeyObj, privateKey, address)
-
-const _privateKey = privateKeyObj.privateKey.toString('base64')
-const jsonKey = JSON.stringify({ Type: 'secp256k1', 'PrivateKey': _privateKey })
-console.log(mnemonic, jsonKey, bin2hex(jsonKey))
+const privateKeyObj = filecoinSigner.keyDerive(mnemonic, createPath(networkCode, 0), '')
+const jsonKey = JSON.stringify({ Type: 'secp256k1', 'PrivateKey': privateKeyObj.private_base64 })
+console.log(mnemonic, privateKeyObj.address, jsonKey, bin2hex(jsonKey))
 
 function bin2hex(bin){
   var i = 0, l = bin.length, chr, hex = ''
